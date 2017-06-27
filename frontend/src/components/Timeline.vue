bfd05dfead079d99986e2bf00b529f26b77bfcff
p1616294 <test@iut.fr>
2017-06-27 15:54:08 +0200

    étape 9 objet Utilisateur

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -49,7 +49,6 @@ export default {
 
 
 
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
--- frontend/src/components/Tweet.vue
+++ frontend/src/components/Tweet.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-      <div> {{ tweet.auteur }} : {{ tweet.contenu }} </div>
+      <div> {{ tweet.auteur.nom }} {{ tweet.auteur.prenom }} <b>@{{ tweet.auteur.handle }}</b> : {{ tweet.contenu }} </div>
   </div>
 </template>
 
